f5ecbe78662699b2aa159d2c60d7192c4ca6b127
tia maryani <tyamaryani.7@gmail.com>
2025-04-18 15:09:02 +0700

    add table post_media

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ namespace Database\Seeders;
 use App\Models\Client;
 use App\Models\Layanan;
 use App\Models\Pegawai;
+use App\Models\PostMedia;
 use App\Models\User;
 use App\Models\UserRole;
 use App\Models\SocialMedia;
@@ -72,6 +73,28 @@ class DatabaseSeeder extends Seeder
             'caption' => 'bla',
             'status' => 0,
             'client_id' => 1
+        ]);
+
+        SocialMedia::create([
+            'content' => 'https://www.logoai.com/uploads/output/2025/03/03/43ec017c8de8a3a36a05c343a452378c.jpg',
+            'caption' => 'bla',
+            'status' => 0,
+            'client_id' => 1
+        ]);
+
+        PostMedia::create([
+            'post_id' => 1,
+            'post' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1200px-LEGO_logo.svg.png',
+        ]);
+
+        PostMedia::create([
+            'post_id' => 1,
+            'post' => 'https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg',
+        ]);
+
+        PostMedia::create([
+            'post_id' => 1,
+            'post' => 'https://www.logoai.com/uploads/output/2025/03/03/a4b5f9ac9d06e5b7b1ed4bb6d9b573a4.jpg',
         ]);
     }
 }

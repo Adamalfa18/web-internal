5387bfa3e1e46789ebaa41aecb7ef832e24d2118
Adamalfa18 <adamalfa18@gmail.com>
2025-04-25 10:47:15 +0700

    Tambahkan code untuk Sa show dan Edit

--- resources/views/marketlab/divisi-sa/index.blade.php
+++ resources/views/marketlab/divisi-sa/index.blade.php
@@ -119,18 +119,220 @@
                     <a href="#"><i class="far fa-bookmark"></i> SAVED</a>
                     <a href="#"><i class="far fa-user"></i> TAGGED</a>
                 </div>
-
                 <div class="gallery">
                     @forelse ($post_medias as $media)
                         <div class="gallery-item">
-                            <a href="{{ asset('storage/media/' . $media->post) }}" target="_blank">
-                                <img src="{{ asset('storage/media/' . $media->post) }}" alt="Social Media" class="img-fluid">
+                            <a href="#" data-bs-toggle="modal" data-bs-target="#mediaModal" target="_blank">
+                                <img src="{{ asset('storage/media/' . $media->post) }}" alt="Social Media"
+                                    class="img-fluid">
                             </a>
                         </div>
                     @empty
                         <p>Belum ada media.</p>
                     @endforelse
-                </div>                                              
+                </div>
+                <div class="modal fade" id="mediaModal" tabindex="-1" role="dialog"
+                    aria-labelledby="mediaModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="mediaModalLabel">Tambah Data Client</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <div class="row form-marketing" enctype="multipart/form-data">
+                                <div class="col-lg-6">
+                                    <div id="carouselExampleIndicators" class="carousel slide"
+                                        data-bs-ride="carousel">
+                                        <div class="carousel-indicators">
+                                            <button type="button" data-bs-target="#carouselExampleIndicators"
+                                                data-bs-slide-to="0" class="active" aria-current="true"
+                                                aria-label="Slide 1"></button>
+                                            <button type="button" data-bs-target="#carouselExampleIndicators"
+                                                data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                            <button type="button" data-bs-target="#carouselExampleIndicators"
+                                                data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                        </div>
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img src="https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=80"
+                                                    class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=80"
+                                                    class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="https://images.unsplash.com/photo-1552793494-111afe03d0ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=80"
+                                                    class="d-block w-100" alt="...">
+                                            </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button"
+                                            data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button"
+                                            data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <form>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlTextarea1"
+                                                        class="form-label">Content</label>
+                                                    <textarea class="form-control" id="exampleFormControlTextarea1"
+                                                        placeholder="Ini Adalah Content Untuk di masukan ke dalam IG" rows="3" disabled></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlTextarea1"
+                                                        class="form-label">Note</label>
+                                                    <textarea class="form-control" id="exampleFormControlTextarea1"
+                                                        placeholder="Ini Adalah Note Dari Cliente jika Ada Revisi" rows="3" disabled></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    @php
+                                                        $status = 2; // Ganti dengan nilai status dari database atau data yang kamu passing
+                                                    @endphp
+
+                                                    <label>Status:</label>
+                                                    @if ($status == 1)
+                                                        <span class="badge badge-success">Status Di Acc</span>
+                                                    @elseif($status == 2)
+                                                        <span class="badge badge-warning">Status Revisi</span>
+                                                    @elseif($status == 3)
+                                                        <span class="badge badge-danger">Status Ditolak</span>
+                                                    @else
+                                                        <span class="badge badge-secondary">Status Tidak
+                                                            Diketahui</span>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary"
+                                                            data-toggle="modal" data-target="#editSAModal">
+                                                            Edit SA
+                                                        </button>
+                                                        <!-- Modal -->
+                                                        <div class="modal fade" id="editSAModal" tabindex="-1"
+                                                            role="dialog" aria-labelledby="editSAModalLabel"
+                                                            aria-hidden="true">
+                                                            <div class="modal-dialog" role="document">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h5 class="modal-title" id="editSAModalLabel">
+                                                                            Edit SA</h5>
+                                                                        <button type="button" class="close"
+                                                                            data-dismiss="modal" aria-label="Tutup">
+                                                                            <span aria-hidden="true">&times;</span>
+                                                                        </button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        <!-- Form atau isi modal -->
+                                                                        <form class="form-marketing"
+                                                                            action="{{ route('divisi-sa.store', ['client_id' => $client_id]) }}"
+                                                                            method="POST"
+                                                                            enctype="multipart/form-data">
+                                                                            @csrf
+
+                                                                            <div class="row">
+                                                                                <div class="col-md-12">
+                                                                                    <div class="mb-3">
+                                                                                        <label for="created_at"
+                                                                                            class="form-label">Caption</label>
+                                                                                        <textarea class="form-control" name="caption" id="caption" required></textarea>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="row">
+                                                                                <div class="col-md-12">
+                                                                                    <div class="mb-3">
+                                                                                        <label for="content"
+                                                                                            class="form-label">Konten
+                                                                                            (Teks)</label>
+                                                                                        <textarea class="form-control" name="content" id="content" required></textarea>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="row">
+                                                                                <div class="col-md-12">
+                                                                                    <div class="mb-3">
+                                                                                        <label for="created_at"
+                                                                                            class="form-label">Tanggal
+                                                                                            Upload</label>
+                                                                                        <input type="date"
+                                                                                            class="form-control"
+                                                                                            name="created_at"
+                                                                                            id="created_at" required>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="row">
+                                                                                <div class="col-md-12">
+                                                                                    <div class="mb-3">
+                                                                                        <label for="content"
+                                                                                            class="form-label">Upload
+                                                                                            Gambar / Video</label>
+                                                                                        <input type="file"
+                                                                                            class="form-control d-none"
+                                                                                            id="content_media"
+                                                                                            name="content_media[]"
+                                                                                            accept=".jpg, .jpeg, .png, .gif, .mp4, .mov, .webm"
+                                                                                            multiple>
+                                                                                        <button type="button"
+                                                                                            class="btn btn-primary"
+                                                                                            id="add-file-btn">Add
+                                                                                            Gambar</button>
+                                                                                    </div>
+                                                                                    <div id="preview-container"
+                                                                                        class="row mt-3"></div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="modal-footer">
+                                                                                <button type="button"
+                                                                                    class="btn btn-secondary"
+                                                                                    data-dismiss="modal">Close</button>
+                                                                                <button type="submit"
+                                                                                    class="btn btn-primary">Simpan</button>
+                                                                            </div>
+
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>

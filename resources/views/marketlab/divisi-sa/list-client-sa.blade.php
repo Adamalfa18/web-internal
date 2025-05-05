3b9c1d381539b82693aae921ff1403bde594a887
Adamalfa18 <adamalfa18@gmail.com>
2025-05-05 10:44:13 +0700

    Edit Liast Sa Search

--- resources/views/marketlab/divisi-sa/list-client-sa.blade.php
+++ resources/views/marketlab/divisi-sa/list-client-sa.blade.php
@@ -15,6 +15,20 @@
                                             <h6 class="font-weight-semibold text-lg mb-0">Data Client SA</h6>
                                             <p class="text-sm">Berikut Adalah List Client SA</p>
                                         </div>
+                                        <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                                            <div class="input-group">
+                                                <span class="input-group-text text-body bg-white border-end-0">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16px"
+                                                        height="16px" fill="none" viewBox="0 0 24 24"
+                                                        stroke-width="1.5" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                                    </svg>
+                                                </span>
+                                                <input type="text" id="search-client" class="form-control ps-0"
+                                                    placeholder="Search by Brand">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="card-body px-0 py-0">
@@ -41,69 +55,72 @@
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                            @foreach ($clients->filter(function ($client) {
-                                                    return $client->layanans->contains('id', 2);
-                                                }) as $client)
-                                                <tr>
-                                                    <td class="align-middle text-center">
-                                                        {{ $loop->iteration }}
-                                                    </td>
-                                                    
-                                                    <td class="client-name-style">
-                                                        <div class="d-flex px-2 py-1">
-                                                            <div class="d-flex flex-column justify-content-center ms-1">
-                                                                <h6 class="mb-0 text-sm font-weight-semibold">
-                                                                    {{ $client->nama_brand }}
-                                                                </h6>
+                                                @foreach ($clients->filter(function ($client) {
+        return $client->layanans->contains('id', 2);
+    }) as $client)
+                                                    <tr>
+                                                        <td class="align-middle text-center">
+                                                            {{ $loop->iteration }}
+                                                        </td>
+
+                                                        <td class="client-name-style">
+                                                            <div class="d-flex px-2 py-1">
+                                                                <div
+                                                                    class="d-flex flex-column justify-content-center ms-1">
+                                                                    <h6 class="mb-0 text-sm font-weight-semibold">
+                                                                        {{ $client->nama_brand }}
+                                                                    </h6>
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                    </td>
-                                                    <td class="client-name-style">
-                                                        <p class="text-sm text-dark font-weight-semibold mb-0">
-                                                            {{ $client->nama_client }}</p>
-                                                    </td>
-                                                    <td class="align-middle text-center text-sm">
-                                                    @switch($client->status_layanan)
-                                                        @case(1)
-                                                            <span
-                                                                class="badge badge-sm border border-success text-success badge-aktif">Aktif
-                                                            </span>
-                                                        @break
+                                                        </td>
+                                                        <td class="client-name-style">
+                                                            <p class="text-sm text-dark font-weight-semibold mb-0">
+                                                                {{ $client->nama_client }}</p>
+                                                        </td>
+                                                        <td class="align-middle text-center text-sm">
+                                                            @switch($client->status_layanan)
+                                                                @case(1)
+                                                                    <span
+                                                                        class="badge badge-sm border border-success text-success badge-aktif">Aktif
+                                                                    </span>
+                                                                @break
 
-                                                        @case(2)
-                                                            <span
-                                                                class="badge badge-sm border border-warning text-warning badge-pending">Pending
-                                                            </span>
-                                                        @break
+                                                                @case(2)
+                                                                    <span
+                                                                        class="badge badge-sm border border-warning text-warning badge-pending">Pending
+                                                                    </span>
+                                                                @break
 
-                                                        @case(3)
-                                                            <span
-                                                                class="badge badge-sm border border-danger text-danger badge-paid">Paid
+                                                                @case(3)
+                                                                    <span
+                                                                        class="badge badge-sm border border-danger text-danger badge-paid">Paid
+                                                                    </span>
+                                                                @break
+                                                            @endswitch
+                                                        </td>
+                                                        <td class="align-middle text-center">
+                                                            <span class="text-secondary text-sm font-weight-normal">
+                                                                {{ $client->date_in }}
                                                             </span>
-                                                        @break
-                                                    @endswitch
-                                                    </td>
-                                                    <td class="align-middle text-center">
-                                                        <span class="text-secondary text-sm font-weight-normal">
-                                                            {{ $client->date_in }}
-                                                        </span>
-                                                    </td>
-                                                    <td class="align-middle">
-                                                        {{-- Tampilkan data bulanan --}}
-                                                        <a href="{{ route('divisi-sa.index', ['client_id' => $client->id]) }}" type="button"
-                                                            class="btn btn-info text-secondary font-weight-bold text-xs active-client"
-                                                            data-bs-toggle="tooltip" data-bs-title="Laporan Bulanan">
-                                                            <svg width="20" height="20"
-                                                                xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" strokeWidth={1.5}
-                                                                stroke="currentColor" className="size-6">
-                                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                                                            </svg>
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
+                                                        </td>
+                                                        <td class="align-middle">
+                                                            {{-- Tampilkan data bulanan --}}
+                                                            <a href="{{ route('divisi-sa.index', ['client_id' => $client->id]) }}"
+                                                                type="button"
+                                                                class="btn btn-info text-secondary font-weight-bold text-xs active-client"
+                                                                data-bs-toggle="tooltip"
+                                                                data-bs-title="Laporan Bulanan">
+                                                                <svg width="20" height="20"
+                                                                    xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                    viewBox="0 0 24 24" strokeWidth={1.5}
+                                                                    stroke="currentColor" className="size-6">
+                                                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                                                        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                                                </svg>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
                                             </tbody>
                                         </table>
                                     </div>
@@ -116,4 +133,18 @@
             </div>
         </div>
     </main>
+    <!-- jQuery (if not already loaded) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Real-time Search Script -->
+    <script>
+        $(document).ready(function() {
+            $('#search-client').on('keyup', function() {
+                var value = $(this).val().toLowerCase();
+                $('table tbody tr').filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
 </x-app-layout>
